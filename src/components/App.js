c83671a2c73d14cb753ea3e7db9ce02fd300b4ba
maximostrovskiy86 <maxim.ostrovskiy86@gmail.com>
2024-11-07 13:39:29 +0100

    chore: added component SharedLayout

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,9 @@
+import React, {useState} from "react";
 import {Routes, Route} from "react-router-dom";
-import Container from "./container";
+import SharedLayout from "./sharedLayout";
 import Home from '../pages/home';
-import Navigation from './navigation';
 import Movies from "../pages/movies";
 import NotFound from "../pages/NotFound";
-import React, {useState} from "react";
 import MovieDetails from "../pages/movieDetails";
 import Cast from "./cast";
 import Reviews from "./reviews";
@@ -15,18 +14,17 @@ function App() {
 	const [reviews, setReviews] = useState(null);
 	
 	return (
-		<Container>
-			<Navigation/>
-			<Routes>
-				<Route path="/" element={<Home/>}/>
+		<Routes>
+			<Route path="/" element={<SharedLayout/>}>
+				<Route index path="/" element={<Home/>}/>
 				<Route path="/movies" element={<Movies/>}/>
 				<Route path="/movies/:movieId" element={<MovieDetails setCast={setCast} setReviews={setReviews}/>}>
 					<Route path="cast" element={<Cast casts={casts}/>}/>
 					<Route path="reviews" element={<Reviews reviews={reviews}/>}/>
 				</Route>
 				<Route path="*" element={<NotFound/>}/>
-			</Routes>
-		</Container>
+			</Route>
+		</Routes>
 	);
 }
 
